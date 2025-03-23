31af4f589ce47ac4bc63526cb2e2dde37f2ed8ea
Abdelmadjid1506 <madjid.kessal@outlook.fr>
2025-03-23 16:13:52 +0100

    firebaseAuth implementation

---
+++ frontend/src/pages/MailConfirmationPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function MailConf(){
+    return (
+        <div>
+            <h1>Veuillez confirmer votre mail pour pouvoir vous connecter</h1>
+        </div>
+    )
+}
